a44259303d8c4b5d6e7f88dfc7c8dfc2f15c3355
sanderv123 <sndrvlttrs@gmail.com>
2021-12-12 17:20:37 +0100

    day 12

--- AdventOfCode/year2021/day12/Day12Solver.cs
+++ AdventOfCode/year2021/day12/Day12Solver.cs
@@ -1,26 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode.year2021.day11
 {
     public class Day12Solver
     {
-        private const string inputLocation = "D:/CSharpProjects/AdventOfCode/AdventOfCode/AdventOfCode/year2021/day11/day11Input.txt";
+        private const string inputLocation = "D:/CSharpProjects/AdventOfCode/AdventOfCode/AdventOfCode/year2021/day12/day12Input.txt";
+
+        private const string nonExistingNode = "afsdflkajsd;fikjhasdlf";
 
         public int SolvePart1()
         {
-            return -1;
+            var nodes = ParseFromFile(inputLocation);
+            var paths = this.GetPaths(nodes, "start", new List<string>(), nonExistingNode);
+            return paths.Count();
         }
 
         public int SolvePart2()
         {
-            return -1;
+            var nodes = ParseFromFile(inputLocation);
+            var paths = this.GetPaths(nodes, "start", new List<string>(), null);
+            return paths.Count();
         }
 
-        private List<string> ParseFromFile(string fileLocation)
+        private List<List<string>> GetPaths(Dictionary<string, List<string>> allNodes, string currentNode, List<string> visitedNodes, string allowAdditionalVisitNode)
+        {
+            visitedNodes.Add(currentNode);
+
+            if (currentNode == "end")
+            {
+                //If we allowed for an additional visit but we haven't used the additional visit, don't return the result as it is a duplicate
+                if (allowAdditionalVisitNode == null || allowAdditionalVisitNode == nonExistingNode)
+                {
+                    return new List<List<string>> { visitedNodes };
+                }
+            }
+
+            var result = new List<List<string>>();
+            var connections = allNodes[currentNode];
+            foreach (var connection in connections)
+            {
+                //Check if the node can be visited
+                if (this.IsLargeNode(connection))
+                {
+                    //Visit it
+                    result.AddRange(this.GetPaths(allNodes, connection, new List<string>(visitedNodes), allowAdditionalVisitNode));
+                }
+                else
+                {
+                    //If we can still visit the small node, visit it
+                    if (!visitedNodes.Contains(connection))
+                    {
+                        //Don't allow for another visit
+                        result.AddRange(this.GetPaths(allNodes, connection, new List<string>(visitedNodes), allowAdditionalVisitNode));
+                        //Allow for another visit
+                        if (allowAdditionalVisitNode == null && connection != "start" && connection != "end")
+                        {
+                            result.AddRange(this.GetPaths(allNodes, connection, new List<string>(visitedNodes), string.Copy(connection)));
+                        }
+                    }
+                    //If we can visit the node again
+                    else if (connection == allowAdditionalVisitNode)
+                    {
+                        result.AddRange(this.GetPaths(allNodes, connection, new List<string>(visitedNodes), nonExistingNode));
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private bool IsLargeNode(string node)
+        {
+            return node.All(c => char.IsUpper(c));
+        }
+
+        private Dictionary<string, List<string>> ParseFromFile(string fileLocation)
         {
             var allText = System.IO.File.ReadAllText(fileLocation);
 
@@ -29,7 +85,29 @@ namespace AdventOfCode.year2021.day11
                 StringSplitOptions.None
             );
 
-            return lines.ToList();
+            var result = new Dictionary<string, List<string>>();
+            foreach (var line in lines)
+            {
+                var fromTo = line.Split('-');
+                var from = fromTo[0];
+                var to = fromTo[1];
+                //Every connection is bi-directional. Therefore, add connections A -> B as well as B -> A
+                if (!result.TryGetValue(from, out var connectionsTo))
+                {
+                    connectionsTo = new List<string>();
+                    result.Add(from, connectionsTo);
+                }
+                connectionsTo.Add(to);
+
+                if (!result.TryGetValue(to, out var connectionsFrom))
+                {
+                    connectionsFrom = new List<string>();
+                    result.Add(to, connectionsFrom);
+                }
+                connectionsFrom.Add(from);
+            }
+
+            return result;
         }
 
     }
