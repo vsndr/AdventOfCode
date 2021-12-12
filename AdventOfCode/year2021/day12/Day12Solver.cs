0b570d2a1da8b152c56445dcb6950856c463107e
sanderv123 <sndrvlttrs@gmail.com>
2021-12-12 19:12:09 +0100

    bla

--- AdventOfCode/year2021/day12/Day12Solver.cs
+++ AdventOfCode/year2021/day12/Day12Solver.cs
@@ -35,18 +35,13 @@ namespace AdventOfCode.year2021.day11
             var connections = allNodes[currentNode];
             foreach (var connection in connections)
             {
-                //if it's a large node
-                if (this.IsLargeNode(connection))
-                {
-                    result.AddRange(this.GetPaths(allNodes, connection, new List<string>(visitedNodes), canVisitSmallNodeTwice));
-                }
-                //If it's a small node that hasn't been visited before
-                else if (!visitedNodes.Contains(connection))
+                //if it's a large node or a small node that hasn't been visited before
+                if (this.IsLargeNode(connection) || !visitedNodes.Contains(connection))
                 {
                     result.AddRange(this.GetPaths(allNodes, connection, new List<string>(visitedNodes), canVisitSmallNodeTwice));
                 }
                 //If it's allowed to visit the node a second time
-                else if (canVisitSmallNodeTwice && connection != "start" && connection != "end")
+                else if (canVisitSmallNodeTwice && connection != "start")
                 {
                     result.AddRange(this.GetPaths(allNodes, connection, new List<string>(visitedNodes), false));
                 }
