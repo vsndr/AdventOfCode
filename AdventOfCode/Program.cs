138d348412c21cc43f7ad6c4c22d676331bc42a1
sanderv123 <sndrvlttrs@gmail.com>
2021-12-14 11:39:29 +0100

    day 14: One steaming pile of garbage

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -17,7 +17,7 @@ namespace AdventOfCode
     {
         public static void Main(string[] args)
         {
-            var solver = new Day13Solver();
+            var solver = new Day14Solver();
             var solution = solver.SolvePart2();
 
             System.Diagnostics.Debug.WriteLine(solution);
--- AdventOfCode/year2021/day14/Day14Solver.cs
+++ AdventOfCode/year2021/day14/Day14Solver.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode.year2021.day11
 {
@@ -10,17 +8,135 @@ namespace AdventOfCode.year2021.day11
     {
         private const string inputLocation = "D:/CSharpProjects/AdventOfCode/AdventOfCode/AdventOfCode/year2021/day14/day14Input.txt";
 
-        public int SolvePart1()
+        //TODO: This code was rushed and needs some rewriting.
+
+        public long SolvePart1()
         {
-            return -1;
+            (var polymer, var insertions) = ParseFromFile(inputLocation);
+            var pairsAfterInsertion = PerformSteps(polymer, insertions, 10);
+            var charCounts = CountCharactersAfterInsertions(pairsAfterInsertion).OrderByDescending(x => x.Value);
+            return charCounts.First().Value - charCounts.Last().Value;      //TODO: Sometimes there's some rounding error occuring here that's giving an answer that's 1 too high
         }
 
-        public int SolvePart2()
+        public long SolvePart2()
         {
-            return -1;
+            (var polymer, var insertions) = ParseFromFile(inputLocation);
+            var pairsAfterInsertion = PerformSteps(polymer, insertions, 40);
+            var charCounts = CountCharactersAfterInsertions(pairsAfterInsertion).OrderByDescending(x => x.Value);
+            return charCounts.First().Value - charCounts.Last().Value;      //TODO: Sometimes there's some rounding error occuring here that's giving an answer that's 1 too high
         }
 
-        private List<string> ParseFromFile(string fileLocation)
+        private Dictionary<char, long> CountCharactersAfterInsertions(Dictionary<(char left, char right), long> allPairs)
+        {
+            var result = new Dictionary<char, long>();
+            foreach (var pair in allPairs)
+            {
+                if (result.TryGetValue(pair.Key.left, out var amount))
+                {
+                    result[pair.Key.left] = amount + pair.Value;
+                }
+                else
+                {
+                    result.Add(pair.Key.left, pair.Value);
+                }
+
+                if (result.TryGetValue(pair.Key.right, out amount))
+                {
+                    result[pair.Key.right] = amount + pair.Value;
+                }
+                else
+                {
+                    result.Add(pair.Key.right, pair.Value);
+                }
+            }
+            //Everything is counted twice, except the starting and the ending character, which is counted twice 1 less time
+            var keys = new List<char>(result.Keys);
+            foreach (var key in keys)
+            {
+                var amount = result[key];
+                if (key == 'N' || key == 'B')
+                {
+                    amount++;
+                }
+
+                result[key] = amount / 2;
+            }
+
+            return result;
+        }
+
+        private Dictionary<(char left, char right), long> PerformSteps(string polymer, List<(char left, char right, char middle)> insertionRules, int steps)
+        {
+            var pairs = GetPairs(polymer);
+            for(int i=0; i<steps; i++)
+            {
+                pairs = GetPairsAfterInsertions(pairs, insertionRules);
+            }
+            return pairs;
+        }
+
+        private Dictionary<(char left, char right), long> GetPairsAfterInsertions(Dictionary<(char left, char right), long> pairs, List<(char left, char right, char middle)> insertionRules)
+        {
+            var insertionRulesDuplicate = new List<(char left, char right, char middle)>(insertionRules);
+            var pairsToAdd = new Dictionary<(char left, char right), long>();
+            var pairsToRemove = new HashSet<(char left, char right)>();
+            for (int i= insertionRulesDuplicate.Count-1; i>=0; i--)
+            {
+                var insertionRule = insertionRulesDuplicate[i];
+                var pair = (insertionRule.left, insertionRule.right);
+                if (pairs.TryGetValue(pair, out var amount))
+                {
+                    pairsToRemove.Add(pair);
+                    AddPair(pairsToAdd, (pair.left, insertionRule.middle), amount);
+                    AddPair(pairsToAdd, (insertionRule.middle, pair.right), amount);
+                }
+                insertionRulesDuplicate.RemoveAt(i);
+            }
+
+            foreach (var pair in pairsToRemove)
+            {
+                pairs.Remove(pair);
+            }
+
+            foreach (var pair in pairsToAdd)
+            {
+                AddPair(pairs, pair.Key, pair.Value);
+            }
+
+            return pairs;
+        }
+
+        private void AddPair(Dictionary<(char left, char right), long> pairs, (char left, char right) newPair, long amount)
+        {
+            if (pairs.TryGetValue(newPair, out var oldAmount))
+            {
+                pairs[newPair] = (oldAmount + amount);
+            }
+            else
+            {
+                pairs.Add(newPair, amount);
+            }
+        }
+
+        private Dictionary<(char left, char right), long> GetPairs(string polymer)
+        {
+            var result = new Dictionary<(char left, char right), long>();
+            for (int i=1; i<polymer.Length; i++)
+            {
+                var pair = (polymer[i-1], polymer[i]);
+                if (!result.TryGetValue(pair, out var amount))
+                {
+                    result.Add(pair, 1);
+                }
+                else
+                {
+                    result[pair] = ++amount;
+                }
+            }
+            return result;
+        }
+
+        private (string polymer, List<(char left, char right, char middle)> insertions) ParseFromFile(string fileLocation)
         {
             var allText = System.IO.File.ReadAllText(fileLocation);
 
@@ -29,7 +145,21 @@ namespace AdventOfCode.year2021.day11
                 StringSplitOptions.None
             );
 
-            return lines.ToList();
+            var polymer = lines[0].Trim();
+            var insertions = new List<(char left, char right, char middle)>();
+            for (int i=1; i<lines.Count(); i++)
+            {
+                if (string.IsNullOrEmpty(lines[i]))
+                {
+                    continue;
+                }
+                var pairInsertion = lines[i].Split(new string[] { "->" }, StringSplitOptions.None);
+                var pair = pairInsertion[0].Trim();
+                var insertion = pairInsertion[1].Trim();
+                insertions.Add((pair[0], pair[1], insertion[0]));
+            }
+
+            return (polymer, insertions);
         }
 
     }
