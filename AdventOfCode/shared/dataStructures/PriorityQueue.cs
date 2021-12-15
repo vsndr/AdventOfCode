16745c2bf3baf34d429af9971f920070525c67e0
sanderv123 <sndrvlttrs@gmail.com>
2021-12-15 22:27:07 +0100

    Add heuristics for pathfinding

---
+++ AdventOfCode/shared/dataStructures/PriorityQueue.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.shared.dataStructures
+{
+    public class PriorityQueue<T> where T : struct
+    {
+        private SortedDictionary<int, List<T>> queue = new SortedDictionary<int, List<T>>();
+
+        public void Add(T item, int priority)
+        {
+            if (this.queue.TryGetValue(priority, out var currentItems))
+            {
+                currentItems.Add(item);
+            }
+            else
+            {
+                queue.Add(priority, new List<T> { item });
+            }
+        }
+
+        public T Pop()
+        {
+            var highestPrioItems = queue.FirstOrDefault();
+            if (highestPrioItems.Value != null)
+            {
+                var firstHighestPrioItem = highestPrioItems.Value.First();
+                highestPrioItems.Value.RemoveAt(0);
+                if (!highestPrioItems.Value.Any())
+                {
+                    queue.Remove(highestPrioItems.Key);
+                }
+                return firstHighestPrioItem;
+            }
+
+            throw new System.Exception("No items to pop");
+        }
+
+
+        public bool TryPop(out T? output)
+        {
+            var highestPrioItems = queue.FirstOrDefault();
+            if (highestPrioItems.Value != null)
+            {
+                output = highestPrioItems.Value.First();
+                highestPrioItems.Value.RemoveAt(0);
+                if (!highestPrioItems.Value.Any())
+                {
+                    queue.Remove(highestPrioItems.Key);
+                }
+                return true;
+            }
+
+            output = null;
+            return false;
+        }
+
+    }
+}
--- AdventOfCode/year2021/day15/Day15Solver.cs
+++ AdventOfCode/year2021/day15/Day15Solver.cs
@@ -12,7 +12,7 @@ namespace AdventOfCode.year2021.day11
         public int SolvePart1()
         {
             var map = this.ParseFromFile(inputLocation);
-            (_, var cost) = this.FindShortestPath(map, new Point(0, 0), new Point(map.GetLength(0)-1, map.GetLength(1)-1));
+            (_, var cost) = this.FindShortestPathASstar(map, new Point(0, 0), new Point(map.GetLength(0)-1, map.GetLength(1)-1));
             return cost;
         }
 
@@ -20,16 +20,74 @@ namespace AdventOfCode.year2021.day11
         {
             var map = this.ParseFromFile(inputLocation);
             var enlargedMap = this.duplicateMap(map, 5);
-            (_, var cost) = this.FindShortestPath(enlargedMap, new Point(0, 0), new Point(enlargedMap.GetLength(0) - 1, enlargedMap.GetLength(1) - 1));
+            (_, var cost) = this.FindShortestPathASstar(enlargedMap, new Point(0, 0), new Point(enlargedMap.GetLength(0) - 1, enlargedMap.GetLength(1) - 1));
             return cost;
         }
 
-
-        private (List<Point> path, int cost) FindShortestPath(int[,] map, Point start, Point target)
+        private (List<Point> path, int cost) FindShortestPathASstar(int[,] map, Point start, Point target)
         {
             var rows = map.GetLength(0);
             var columns = map.GetLength(1);
-            var spatialSet = new Dictionary<Point, (List<Point> points, int distance)>() { { start, (new List<Point> { start }, 0) } };
+            var spatialMap = new Dictionary<Point, (List<Point> points, int distance)>() { { start, (new List<Point> { start }, 0) } };
+            var undiscoveredPoints = new PriorityQueue<Point>();
+            undiscoveredPoints.Add(start, 0);
+
+            while (undiscoveredPoints.TryPop(out var undiscoveredPoint))
+            {
+                //Stop if we've reached the target point
+                if (undiscoveredPoint.Value.a == target.a && undiscoveredPoint.Value.b == target.b)
+                {
+                    break;
+                }
+
+                //Get the path to the currentpoint that we're going to expand
+                (var visitedPoints, var distanceToPoint) = spatialMap[undiscoveredPoint.Value];
+
+                //Discover new points
+                var neighborPoints = new Point[] { 
+                    new Point(undiscoveredPoint.Value.a - 1, undiscoveredPoint.Value.b),
+                    new Point(undiscoveredPoint.Value.a + 1, undiscoveredPoint.Value.b),
+                    new Point(undiscoveredPoint.Value.a, undiscoveredPoint.Value.b - 1),
+                    new Point(undiscoveredPoint.Value.a, undiscoveredPoint.Value.b + 1)
+                };
+
+                foreach (var neighborPoint in neighborPoints)
+                {
+                    //Check if the point actually falls on the grid
+                    if (neighborPoint.a >= 0 && neighborPoint.a < rows && neighborPoint.b >= 0 && neighborPoint.b < columns)
+                    {
+                        var distanceToNeighborPoint = distanceToPoint + map[neighborPoint.a, neighborPoint.b];
+                        //If the spatial map does not contain the distance to the newly discovered point yet
+                        //OR the distance to the newly discovered point is smaller than the path that was already discovered to that point
+                        if (!spatialMap.TryGetValue(neighborPoint, out var currentDistance) || currentDistance.distance > distanceToNeighborPoint)
+                        {
+                            //Update the spatial map
+                            var path = new List<Point>(visitedPoints);
+                            path.Add(neighborPoint);
+                            spatialMap[neighborPoint] = (visitedPoints, distanceToNeighborPoint);
+
+                            //Add the newly discovered point to the priority queue such that new points can be discovered from that point
+                            var priority = CalculateEstimatedCost(neighborPoint, distanceToNeighborPoint, target);
+                            undiscoveredPoints.Add(neighborPoint, priority);
+                        }
+                    }
+                }
+            }
+
+            return spatialMap[target];
+        }
+
+        private int CalculateEstimatedCost(Point point, int pathCost, Point target)
+        {
+            return pathCost + (target.a + target.b - point.a - point.b);
+        }
+        
+
+        private (List<Point> path, int cost) FindShortestPathDijkstra(int[,] map, Point start, Point target)
+        {
+            var rows = map.GetLength(0);
+            var columns = map.GetLength(1);
+            var spatialMap = new Dictionary<Point, (List<Point> points, int distance)>() { { start, (new List<Point> { start }, 0) } };
             var undiscoveredPoints = new HashSet<Point> { start };
 
             while (undiscoveredPoints.Any())
@@ -38,16 +96,17 @@ namespace AdventOfCode.year2021.day11
                 //Discover new points
                 foreach (var point in undiscoveredPoints)
                 {
-                    (var visitedPoints, var distanceToPoint) = spatialSet[point];
+                    (var visitedPoints, var distanceToPoint) = spatialMap[point];
+
 
                     //Left
                     if (point.b - 1 >= 0)
                     {
                         var leftPoint = new Point(point.a, point.b-1);
                         var distanceToLeftPoint = distanceToPoint + map[leftPoint.a, leftPoint.b];
-                        if (!spatialSet.TryGetValue(leftPoint, out var currentDistance) || currentDistance.distance > distanceToLeftPoint)
+                        if (!spatialMap.TryGetValue(leftPoint, out var currentDistance) || currentDistance.distance > distanceToLeftPoint)
                         {
-                            spatialSet[leftPoint] = (new List<Point>(visitedPoints), distanceToLeftPoint);
+                            spatialMap[leftPoint] = (new List<Point>(visitedPoints), distanceToLeftPoint);
                             newDiscoveredPoints.Add(leftPoint);
                         }
                     }
@@ -57,9 +116,9 @@ namespace AdventOfCode.year2021.day11
                     {
                         var rightPoint = new Point(point.a, point.b + 1);
                         var distanceToLeftPoint = distanceToPoint + map[rightPoint.a, rightPoint.b];
-                        if (!spatialSet.TryGetValue(rightPoint, out var currentDistance) || currentDistance.distance > distanceToLeftPoint)
+                        if (!spatialMap.TryGetValue(rightPoint, out var currentDistance) || currentDistance.distance > distanceToLeftPoint)
                         {
-                            spatialSet[rightPoint] = (new List<Point>(visitedPoints), distanceToLeftPoint);
+                            spatialMap[rightPoint] = (new List<Point>(visitedPoints), distanceToLeftPoint);
                             newDiscoveredPoints.Add(rightPoint);
                         }
                     }
@@ -69,9 +128,9 @@ namespace AdventOfCode.year2021.day11
                     {
                         var topPoint = new Point(point.a - 1, point.b);
                         var distanceToLeftPoint = distanceToPoint + map[topPoint.a, topPoint.b];
-                        if (!spatialSet.TryGetValue(topPoint, out var currentDistance) || currentDistance.distance > distanceToLeftPoint)
+                        if (!spatialMap.TryGetValue(topPoint, out var currentDistance) || currentDistance.distance > distanceToLeftPoint)
                         {
-                            spatialSet[topPoint] = (new List<Point>(visitedPoints), distanceToLeftPoint);
+                            spatialMap[topPoint] = (new List<Point>(visitedPoints), distanceToLeftPoint);
                             newDiscoveredPoints.Add(topPoint);
                         }
                     }
@@ -81,9 +140,9 @@ namespace AdventOfCode.year2021.day11
                     {
                         var bottomPoint = new Point(point.a + 1, point.b);
                         var distanceToLeftPoint = distanceToPoint + map[bottomPoint.a, bottomPoint.b];
-                        if (!spatialSet.TryGetValue(bottomPoint, out var currentDistance) || currentDistance.distance > distanceToLeftPoint)
+                        if (!spatialMap.TryGetValue(bottomPoint, out var currentDistance) || currentDistance.distance > distanceToLeftPoint)
                         {
-                            spatialSet[bottomPoint] = (new List<Point>(visitedPoints), distanceToLeftPoint);
+                            spatialMap[bottomPoint] = (new List<Point>(visitedPoints), distanceToLeftPoint);
                             newDiscoveredPoints.Add(bottomPoint);
                         }
                     }
@@ -92,7 +151,7 @@ namespace AdventOfCode.year2021.day11
                 undiscoveredPoints = newDiscoveredPoints;
             }
 
-            return spatialSet[target];
+            return spatialMap[target];
         }
 
         private int[,] duplicateMap(int[,] map, int duplicationCount)
